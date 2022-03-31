4a9e0897d441040560bf57535e49b338bb7aa6f1
Said Moya <smoya@ekoomedia.co>
2022-03-30 22:27:05 -0500

    interpolation Fix speed

--- components/Trackpad/index.js
+++ components/Trackpad/index.js
@@ -4,6 +4,9 @@ import Animated, {
 	withSpring,
 	useAnimatedStyle,
 	useAnimatedGestureHandler,
+	useDerivedValue,
+	runOnJS,
+	withDecay,
 } from "react-native-reanimated"
 import {PanGestureHandler} from "react-native-gesture-handler"
 
@@ -85,18 +88,21 @@ import {SERVER} from "../../contants"
 
 console.log("el server es", SERVER)
 
+const X_DECELERATION_SPEED = 0.7
+const Y_DECELERATION_SPEED = 0.7
+
 function Trackpad() {
 	const socket = io(SERVER, {})
 
-	const handleGesture = (evt) => {
-		let {nativeEvent} = evt
-
-		socket.emit("trackpad", nativeEvent)
+	const handleGesture = (data) => {
+		socket.emit("trackpad", data)
 	}
 
 	const startingPosition = 0
 	const x = useSharedValue(startingPosition)
 	const y = useSharedValue(startingPosition)
+	const translationX = useSharedValue(startingPosition)
+	const translationY = useSharedValue(startingPosition)
 
 	const gestureHandler = useAnimatedGestureHandler({
 		onStart: (_, ctx) => {
@@ -104,14 +110,41 @@ function Trackpad() {
 			ctx.startY = y.value
 		},
 		onActive: (event, ctx) => {
+			//cursor
 			x.value = ctx.startX + event.translationX
 			y.value = ctx.startY + event.translationY
+
+			//socket
+			translationX.value = event.translationX
+			translationY.value = event.translationY
 		},
-		onEnd: (_) => {
+
+		onEnd: ({velocityX, velocityY}) => {
+			//cursor
 			x.value = withSpring(startingPosition)
 			y.value = withSpring(startingPosition)
+
+			//translation interpolation
+			translationX.value = withDecay({
+				velocity: velocityX,
+				deceleration: X_DECELERATION_SPEED,
+				clamp: [-250, 250],
+			})
+			translationY.value = withDecay({
+				velocity: velocityY,
+				deceleration: Y_DECELERATION_SPEED,
+				clamp: [-250, 250],
+			})
 		},
 	})
+
+	useDerivedValue(() => {
+		//send to socket on transation update
+		runOnJS(handleGesture)({
+			translationX: translationX.value / 60,
+			translationY: translationY.value / 70,
+		})
+	}, [translationX.value, translationY.value])
 
 	const animatedStyle = useAnimatedStyle(() => {
 		return {
@@ -147,7 +180,7 @@ function Trackpad() {
 					resizeMode="contain"
 					type="right"
 				/>
-				<PanGestureHandler onGestureEvent={handleGesture}>
+				<PanGestureHandler onGestureEvent={gestureHandler}>
 					<Animated.View style={[animatedStyle]}>
 						<TrackpadArrow
 							source={require("../../assets/hand_trackpad.png")}
